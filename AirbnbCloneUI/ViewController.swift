d0f2a4adb4b26565eec8090e9a0143daba3a7e80
Ruvinda Lahiru <ruvinda3@gmail.com>
2020-07-31 05:26:15 -0700

    Initial Commit

---
+++ AirbnbCloneUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AirbnbCloneUI
+//
+//  Created by Ruvinda Lahiru Samaranayake on 7/31/20.
+//  Copyright © 2020 Ruvinda Lahiru Samaranayake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
